3250c84e0f52ccdc7102cd877245492e3ed7e689
Jasper Post <jasper.post@cloudnation.nl>
2024-09-25 14:34:15 +0200

    fix: added additional optional config for public ip (#10)
    * fix: Added additional optional config for public ip
         Updated readme to match new azurerm version
    
    * fix: formatting

--- main.tf
+++ main.tf
@@ -2,16 +2,22 @@
 resource "azurerm_public_ip" "public" {
   for_each = var.configs
 
-  name                = each.value.name
-  resource_group_name = coalesce(lookup(each.value, "resource_group", null), var.resource_group)
-  location            = coalesce(lookup(each.value, "location", null), var.location)
-  allocation_method   = try(each.value.allocation_method, "Static")
-  sku                 = try(each.value.sku, "Standard")
-  public_ip_prefix_id = lookup(each.value, "prefix", null) != null ? azurerm_public_ip_prefix.prefix[each.key].id : null
-  zones               = each.value.zones
-  sku_tier            = try(each.value.sku_tier, "Regional")
-  ip_version          = try(each.value.ip_version, "IPv4")
-  tags                = try(var.tags, {})
+  name                    = each.value.name
+  resource_group_name     = coalesce(lookup(each.value, "resource_group", null), var.resource_group)
+  location                = coalesce(lookup(each.value, "location", null), var.location)
+  allocation_method       = try(each.value.allocation_method, "Static")
+  sku                     = try(each.value.sku, "Standard")
+  public_ip_prefix_id     = lookup(each.value, "prefix", null) != null ? azurerm_public_ip_prefix.prefix[each.key].id : null
+  zones                   = try(each.value.zones, null)
+  ddos_protection_mode    = try(each.value.ddos_protection_mode, "VirtualNetworkInherited")
+  ddos_protection_plan_id = try(each.value.ddos_protection_plan_id, null)
+  domain_name_label       = try(each.value.domain_name_label, null)
+  edge_zone               = try(each.value.edge_zone, null)
+  idle_timeout_in_minutes = try(each.value.idle_timeout_in_minutes, null)
+  reverse_fqdn            = try(each.value.reverse_fqdn, null)
+  sku_tier                = try(each.value.sku_tier, "Regional")
+  ip_version              = try(each.value.ip_version, "IPv4")
+  tags                    = try(var.tags, {})
 }
 
 resource "azurerm_public_ip_prefix" "prefix" {
