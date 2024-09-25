3e5f06e5af5953ef96953c12e27b61b8aa227d0d
dkool <dkool@dkools-MacBook-Pro.local>
2024-09-25 08:11:20 +0200

    feat!: upgrade azurerm provider to v4 BREAKING CHANGE: * Version 4 of the azurerm provider includes breaking changes.

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -7,7 +7,7 @@ module "naming" {
 
 module "rg" {
   source  = "cloudnationhq/rg/azure"
-  version = "~> 1.0"
+  version = "~> 2.0"
 
   groups = {
     demo = {
@@ -19,7 +19,7 @@ module "rg" {
 
 module "public_ip" {
   source  = "cloudnationhq/pip/azure"
-  version = "~> 1.0"
+  version = "~> 2.0"
 
   configs = {
     pub = {
